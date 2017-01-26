77ae66c732d9d9b4456022f7b23930eaff801c42
radames-rex <radames.rex@gmail.com>
2017-01-26 19:42:09 -0300

    Endpoints: peopleIdPUT/

--- backend/controllers/PublicService.js
+++ backend/controllers/PublicService.js
@@ -9,15 +9,17 @@ exports.peopleIdDELETE = function(args, res, next) {
      * id (String)
      **/
     var db = DBConfig.dbConnect();
-    db.once('open', function(){
-      console.log('Conectado ao MongoDB.');
-      People.remove({"_id":args.id.value},function(err){
-        if(err){
-          return console.error(err);
-          console.dir(p);
-        }
-        DBConfig.dbClose();
-      });
+    db.once('open', function() {
+        console.log('Conectado ao MongoDB.');
+        People.remove({
+            "_id": args.id.value
+        }, function(err) {
+            if (err) {
+                return console.error(err);
+                console.dir(p);
+            }
+            DBConfig.dbClose();
+        });
     });
     res.end();
 }
@@ -28,27 +30,29 @@ exports.peopleIdGET = function(args, res, next) {
      * id (String)
      **/
     var data = {},
-      db = DBConfig.dbConnect();
-    db.once('open', function(){
-      console.log('Conectado ao MongoDB.');
-      People.findOne({"_id":args.id.value},function(err, people){
-        if(err){
-          return console.error(err);
-          console.dir(p);
-        }
-        data['application/json'] = {
-            "name": people.name,
-            "email": people.email,
-            "amigo": people.friend
-        };
-        DBConfig.dbClose();
-        if (Object.keys(data).length > 0) {
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify(data[Object.keys(data)[0]] || {}, null, 2));
-        } else {
-            res.end();
-        }
-      });
+        db = DBConfig.dbConnect();
+    db.once('open', function() {
+        console.log('Conectado ao MongoDB.');
+        People.findOne({
+            "_id": args.id.value
+        }, function(err, people) {
+            if (err) {
+                return console.error(err);
+                console.dir(p);
+            }
+            data['application/json'] = {
+                "name": people.name,
+                "email": people.email,
+                "amigo": people.friend
+            };
+            DBConfig.dbClose();
+            if (Object.keys(data).length > 0) {
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(data[Object.keys(data)[0]] || {}, null, 2));
+            } else {
+                res.end();
+            }
+        });
     });
 
 }
@@ -75,8 +79,30 @@ exports.peopleIdPUT = function(args, res, next) {
      * id (String)
      * people (People_1)
      **/
-    // no response value expected for this operation
-    res.end();
+    var data = {},
+        db = DBConfig.dbConnect();
+    data['application/json'] = {
+        "name": args.people.value.name,
+        "email": args.people.value.email
+    };
+    db.once('open', function() {
+        console.log('Conectado ao MongoDB.');
+        People.findOneAndUpdate({
+            "_id": args.id.value
+        }, data['application/json'], function(err, people) {
+            if (err) {
+                return console.error(err);
+                console.dir(p);
+            }
+            DBConfig.dbClose();
+            if (Object.keys(data).length > 0) {
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(data[Object.keys(data)[0]] || {}, null, 2));
+            } else {
+                res.end();
+            }
+        });
+    });
 }
 
 exports.peopleOPTIONS = function(args, res, next) {
@@ -100,19 +126,19 @@ exports.peoplePOST = function(args, res, next) {
      * people (People)
      **/
     var db = DBConfig.dbConnect();
-    db.once('open', function(){
-      console.log('Conectado ao MongoDB.');
-      var p = new People({
-        name: args.people.value.name,
-        email: args.people.value.email
-      });
-      p.save(function(err, p) {
-        if(err){
-          return console.error(err);
-          console.dir(p);
-        }
-        DBConfig.dbClose();
-      });
+    db.once('open', function() {
+        console.log('Conectado ao MongoDB.');
+        var p = new People({
+            name: args.people.value.name,
+            email: args.people.value.email
+        });
+        p.save(function(err, p) {
+            if (err) {
+                return console.error(err);
+                console.dir(p);
+            }
+            DBConfig.dbClose();
+        });
     });
     res.end();
 }
@@ -122,30 +148,30 @@ exports.peoplesGET = function(args, res, next) {
      * parameters expected in the args:
      **/
     var data = {},
-      arr = [],
-      db = DBConfig.dbConnect();
-    db.once('open', function(){
-      console.log('Conectado ao MongoDB.');
-      People.find().exec(function(err, peoples){
-        if(err){
-          return console.error(err);
-          console.dir(p);
-        }
-        peoples.forEach(function(value){
-          arr.push({
-              "id": value['_id'],
-              "name": value['name']
-          });
+        arr = [],
+        db = DBConfig.dbConnect();
+    db.once('open', function() {
+        console.log('Conectado ao MongoDB.');
+        People.find().exec(function(err, peoples) {
+            if (err) {
+                return console.error(err);
+                console.dir(p);
+            }
+            peoples.forEach(function(value) {
+                arr.push({
+                    "id": value['_id'],
+                    "name": value['name']
+                });
+            });
+            data['application/json'] = arr;
+            DBConfig.dbClose();
+            if (Object.keys(data).length > 0) {
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(data[Object.keys(data)[0]] || {}, null, 2));
+            } else {
+                res.end();
+            }
         });
-        data['application/json'] = arr;
-        DBConfig.dbClose();
-        if (Object.keys(data).length > 0) {
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify(data[Object.keys(data)[0]] || {}, null, 2));
-        } else {
-            res.end();
-        }
-      });
     });
 
 }
